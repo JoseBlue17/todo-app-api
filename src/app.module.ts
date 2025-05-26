65bf63962816629f39cdbac36f6ca3f20a65d7bd
Oliver Velasquez <Oliver.vlz01@gmail.com>
2025-05-26 12:01:55 -0500

    app.module actualizado, elimar elementos innecesarios

--- src/app.module.ts
+++ src/app.module.ts
@@ -18,11 +18,6 @@ import { ClientRouteBuilder } from './shared/utils';
 import { JwtAuthGuard } from './shared/guards/jwt-auth.guard';
 import { MailModule } from './shared/mail';
 import { IdentityModule } from './identity/infrastructure/identity.module';
-import { UserSeeder } from './shared/seed/user.seeder';
-import {
-  User,
-  UserSchema,
-} from './identity/infrastructure/schemas/user.schema';
 
 @Module({
   imports: [
@@ -52,11 +47,9 @@ import {
       inject: [ConfigService],
       useFactory: (config: ConfigService) => config.get('database.mongo'),
     }),
-    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
   controllers: [AppController],
   providers: [
-    UserSeeder,
     AppService,
     { provide: APP_GUARD, useClass: JwtAuthGuard },
     {
