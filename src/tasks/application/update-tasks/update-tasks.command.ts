a084f0525425f5db18d8ace80fd6dbcb98d7ad1c
Oliver Velasquez <Oliver.vlz01@gmail.com>
2025-06-05 15:12:46 -0500

    feat(update-task): change params to tasks

---
+++ src/tasks/application/update-tasks/update-tasks.command.ts
@@ -0,0 +1,13 @@
+import { ICommand } from '@nestjs/cqrs';
+
+export class UpdateTaskCommand implements ICommand {
+  constructor(
+    public readonly title: string,
+    public readonly description: string | null,
+    public readonly completed: boolean | null,
+    public readonly category: string | null,
+    public readonly dueDate: Date | null,
+    public readonly userId: string,
+    public readonly taskId: string,
+  ) {}
+}
