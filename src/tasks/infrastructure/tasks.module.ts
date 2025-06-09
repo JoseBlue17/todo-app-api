84c08d4a2240a22ce69ab48f42ed0f0b35dc3ca7
Oliver Velasquez <Oliver.vlz01@gmail.com>
2025-06-09 12:41:39 -0500

    refactor(tasks): Imports organization

--- src/tasks/infrastructure/tasks.module.ts
+++ src/tasks/infrastructure/tasks.module.ts
@@ -4,12 +4,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 import { CreateTaskHandler } from '../application/create-tasks/create-task.handler';
 import { GetTasksHandler } from '../application/get-tasks/get-tasks.handler';
+import { UpdateTaskHandler } from '../application/update-tasks/update-tasks.handler';
 
 import { TaskRepository } from './repositories/tasks.repositories';
 import { Task, TaskSchema } from './schemas/task.schema';
 import { TaskSearchService } from './services/task-search.service';
 import { TasksController } from './tasks.controller';
-import { UpdateTaskHandler } from '../application/update-tasks/update-tasks.handler';
 
 @Module({
   imports: [
