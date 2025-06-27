f8cd508911f9d2bf45ea88d3b12cfa34a080b806
Oliver Velasquez <Oliver.vlz01@gmail.com>
2025-06-27 15:15:08 -0500

    refactor(swagger): swagger config created

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
-import { ApiExcludeController } from '@nestjs/swagger';
 
 import { AppService } from './app.service';
 import { Public } from './shared/validation';
 
 @Controller()
-@ApiExcludeController()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
